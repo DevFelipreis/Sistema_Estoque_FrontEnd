3e4d8959da041ea036c4da7ef56b6ebf9313153a
DevFelipreis <lfreis.contato@gmail.com>
2024-06-07 17:09:06 -0300

    feat: improve the interface

--- src/scripts/script-create-product.js
+++ src/scripts/script-create-product.js
@@ -1,30 +1,41 @@
 document.getElementById('cadastroForm').addEventListener('submit', async function (event) {
     event.preventDefault();
+    console.log('Formulário enviado');
 
     const formData = new FormData(this);
+    const formObject = {};
+
+    formData.forEach((value, key) => {
+        formObject[key] = value;
+    });
 
     const requestOptions = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(Object.fromEntries(formData))
+        body: JSON.stringify(formObject)
     };
+
     try {
         const response = await fetch('https://sistema-estoque-nsv6.onrender.com/products', requestOptions);
+
         const data = await response.json();
 
         if (response.ok) {
-            alert('Produto cadastrado com sucesso!');
-            document.getElementById('serverResponse').value = data.message;
+            if (data) {
+                const product = JSON.parse(data);
+                const productText = `ID: ${product.id},\nNome: ${product.nome},\nPreço: ${product.preco},\nQuantidade: ${product.quantidade},
+                \nCategoria: ${product.categoria_id},\nDescrição: ${product.descricao}`;
+                alert('Produto criado com sucesso!');
+                document.getElementById('serverResponse').value = productText;
+            }
         } else {
-            throw new Error(data.message || 'Erro ao adicionar o produto');
+            throw new Error(data.message || 'Erro ao cadastrar o produto');
         }
     } catch (error) {
-        alert('Erro ao adicionar o produto: ' + error.message);
-        document.getElementById('serverResponse').value = error.message;
+        console.error('Erro ao cadastrar o produto:', error);
+        alert('Erro ao cadastrar o produto: ' + error.message);
+        document.getElementById('serverResponse').value = 'Erro: ' + error.message;
     }
-
 });
-
-
