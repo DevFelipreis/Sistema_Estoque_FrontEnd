989266b625eaea6e3329dbddc530b1a166b49156
DevFelipreis <lfreis.contato@gmail.com>
2024-06-08 15:59:05 -0300

    feat: improve the interface

--- src/scripts/script-get-product.js
+++ src/scripts/script-get-product.js
@@ -1,37 +1,34 @@
 document.getElementById('listaForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
-    const id = document.getElementById('id').value;
-    const nome = document.getElementById('nome').value;
-    const categoria_id = document.getElementById('categoria_id').value;
-    console.log(id, nome, categoria_id);
+    const id = document.getElementById('id').value.trim();
+    const nome = document.getElementById('nome').value.trim().toLowerCase();
+    const categoria_id = document.getElementById('categoria_id').value.trim();
 
     const queryParams = new URLSearchParams();
     if (id) queryParams.append('id', id);
     if (nome) queryParams.append('nome', nome);
     if (categoria_id) queryParams.append('categoria_id', categoria_id);
+    console.log(queryParams.toString());
 
     try {
         document.getElementById('serverMessage').innerText = '';
-        const response = await fetch(`https://sistema-estoque-nsv6.onrender.com/products?${queryParams.toString()}`,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
+        const response = await fetch(`https://sistema-estoque-nsv6.onrender.com/products?${queryParams.toString()}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         const data = await response.json();
 
         if (response.ok) {
             if (Array.isArray(data) && data.length > 0) {
                 const productsText = data.map(product => {
-                    return `ID: ${product.id},\nNome: ${product.nome},\nPreço: ${product.preco},\nQuantidade: ${product.quantidade},
-                    \nCategoria: ${product.categoria_nome},\nDescrição: ${product.descricao}`;
-                }).join('\n______________________________\n');;
+                    return `ID: ${product.id},\nNome: ${product.nome},\nPreço: ${product.preco},\nQuantidade: ${product.quantidade},\nCategoria: ${product.categoria_nome},\nDescrição: ${product.descricao}`;
+                }).join('\n______________________________\n');
                 document.getElementById('serverResponse').value = productsText;
             } else if (data.id) {
-                const productText = `ID: ${data.id},\nNome: ${data.nome},\nPreço: ${data.preco},\nQuantidade: ${data.quantidade},
-                \nCategoria: ${data.categoria_nome},\nDescrição: ${data.descricao}`;
+                const productText = `ID: ${data.id},\nNome: ${data.nome},\nPreço: ${data.preco},\nQuantidade: ${data.quantidade},\nCategoria: ${data.categoria_nome},\nDescrição: ${data.descricao}`;
                 document.getElementById('serverResponse').value = productText;
             } else {
                 document.getElementById('serverResponse').value = 'Nenhum produto encontrado.';
--- src/scripts/script-update-product.js
+++ src/scripts/script-update-product.js
@@ -1,38 +1,43 @@
-document.getElementById('atualizarForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('atualizarForm').addEventListener('submit', async function (event) {
+        event.preventDefault();
 
-    const formData = new FormData(this);
-    const data = {};
+        const formData = new FormData(this);
+        const data = {};
 
-    formData.forEach((value, key) => {
-        if (value) {
-            data[key] = value;
+        formData.forEach((value, key) => {
+            if (value) {
+                data[key] = value;
+            }
+        });
+
+        const requestOptions = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+
+        try {
+            const serverMessage = document.getElementById('serverMessage');
+            if (serverMessage) {
+                serverMessage.innerText = '';
+            }
+
+            const response = await fetch('https://sistema-estoque-nsv6.onrender.com/products', requestOptions);
+            const responseData = await response.json();
+
+            if (response.ok) {
+                alert('Produto criado com sucesso!');
+                document.getElementById('serverResponse').value = responseData.message;
+
+            } else {
+                throw new Error(responseData.message || 'Erro ao atualizar produto');
+            }
+        } catch (error) {
+            alert('Erro ao atualizar produto: ' + error.message);
+            document.getElementById('serverResponse').value = 'Erro: ' + error.message;
         }
     });
-
-    const requestOptions = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-
-    };
-
-    try {
-        document.getElementById('serverMessage').innerText = '';
-        const response = await fetch('https://sistema-estoque-nsv6.onrender.com/products', requestOptions);
-        const responseData = await response.json();
-
-        if (response.ok) {
-            alert('Produto criado com sucesso!');
-            document.getElementById('serverResponse').value = responseData.message;
-
-        } else {
-            throw new Error(responseData.message || 'Erro ao atualizar produto');
-        }
-    } catch (error) {
-        alert('Erro ao atualizar produto: ' + error.message);
-        document.getElementById('serverResponse').value = 'Erro: ' + error.message;
-    }
 });
