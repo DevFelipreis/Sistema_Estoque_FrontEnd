1049bfcbc512b0d087f2728e41161446f0c68568
DevFelipreis <lfreis.contato.com>
2024-06-25 14:07:19 -0300

    feat

--- src/scripts/script-create-product.js
+++ src/scripts/script-create-product.js
@@ -1,6 +1,5 @@
 document.getElementById('cadastroForm').addEventListener('submit', async function (event) {
     event.preventDefault();
-    console.log('Formulário enviado');
 
     const formData = new FormData(this);
     const formObject = {};
@@ -11,6 +10,19 @@ document.getElementById('cadastroForm').addEventListener('submit', async functio
 
     const token = localStorage.getItem('token');
 
+    if (!token) {
+        alert('Necessário realizar o login para entrar no sistema.');
+        window.location.href = './login-user.html';
+        return;
+    }
+
+    const profissaoId = localStorage.getItem('profissao_id');
+    if (profissaoId !== 'administrador' && profissaoId !== 'gerente' && profissaoId !== 'encarregado') {
+        alert('Necessário realizar o login para entrar no sistema.');
+        window.location.href = './login-user.html';
+        return;
+    }
+
     const requestOptions = {
         method: 'POST',
         headers: {
@@ -20,39 +32,24 @@ document.getElementById('cadastroForm').addEventListener('submit', async functio
         body: JSON.stringify(formObject)
     };
 
-    if (!token) {
-        alert('Necessário realizar o login para entrar no sistema.');
-        window.location.href = './login-user.html';
-        return
-    };   
-
-    localStorage.setItem('profissao_id', data.usuario.profissao);
-
-    if(data.usuario.profissao !== 'administrador' || data.usuario.profissao !== 'gerente' || data.usuario.profissao != 'encarregado') {
-        alert('Necessário realizar o login para entrar no sistema.');
-        window.location.href = './login-user.html';
-        return
-    }
-
     try {
         const response = await fetch('https://sistema-estoque-nsv6.onrender.com/products', requestOptions);
-
         const data = await response.json();
 
-               if (response.ok) {
-            if (data) {
-                const product = JSON.parse(data);
+        if (response.ok) {
+            if (data.productCreated) {
+                const product = JSON.parse(data.productCreated);
                 const productText = `ID: ${product.id},\nNome: ${product.nome},\nPreço: ${product.preco},\nQuantidade: ${product.quantidade},
                 \nCategoria: ${product.categoria_id},\nDescrição: ${product.descricao}`;
                 alert('Produto criado com sucesso!');
                 document.getElementById('serverResponse').value = productText;
-            };
+            }
         } else {
             throw new Error(data.message || 'Erro ao cadastrar o produto');
-        };
+        }
     } catch (error) {
         console.error('Erro ao cadastrar o produto:', error);
         alert('Erro ao cadastrar o produto: ' + error.message);
         document.getElementById('serverResponse').value = 'Erro: ' + error.message;
-    };
+    }
 });
--- src/scripts/script-entry-product.js
+++ src/scripts/script-entry-product.js
@@ -10,32 +10,31 @@ document.getElementById('entradaForm').addEventListener('submit', async function
 
     const token = localStorage.getItem('token');
 
-    const requestOptions = {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-
-        body: JSON.stringify(formObject)
-    };
-
-    localStorage.setItem('profissao_id', data.usuario.profissao);
-
-    if(data.usuario.profissao == 'vendedor' || data.usuario.profissao !== 'estoquista') {
-        alert('Necessário realizar o login para entrar no sistema.');
-        window.location.href = './login-user.html';
-        return
-    }
-
     if (!token) {
         alert('Necessário realizar o login para entrar no sistema.');
         window.location.href = './login-user.html';
-    };
+        return;
+    }
 
     try {
+        const requestOptions = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(formObject)
+        };
+
         const response = await fetch('https://sistema-estoque-nsv6.onrender.com/products', requestOptions);
         const data = await response.json();
+
+        const profissaoId = localStorage.getItem('profissao_id');
+        if (profissaoId !== 'estoquista' && profissaoId === 'vendedor') {
+            alert('Necessário realizar o login para entrar no sistema.');
+            window.location.href = './login-user.html';
+            return;
+        }
 
         if (response.ok) {
             if (data.productEntry) {
@@ -46,12 +45,12 @@ document.getElementById('entradaForm').addEventListener('submit', async function
                 document.getElementById('serverResponse').value = productText;
             } else {
                 document.getElementById('serverResponse').value = 'Nenhum produto encontrado.';
-            };
+            }
         } else {
             throw new Error(data.message || 'Erro ao listar os produtos');
-        };
+        }
     } catch (error) {
         alert('Erro ao listar os produtos: ' + error.message);
         document.getElementById('serverResponse').value = 'Erro: ' + error.message;
-    };
+    }
 });
